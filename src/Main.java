2bfcb8f26d59bcd1ac705f5c7733fc3c0180f461
rmcampigotto <rmcampigotto@gmail.com>
2023-07-31 12:43:01 -0300

    bornDateTests0731

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
 import Pessoa.Pessoa;
+import java.util.Calendar;
 
 public class Main {
     public static void main(String[] args) {
-
+        Calendar cal = Calendar.getInstance();
         Pessoa aluno = new Pessoa();
+        int year = 2010;
 
-        aluno.setAge(10);
+        aluno.setBornDate(year, 6,16); // JULHO = 8 (os meses são +1)
+        aluno.setAge(cal.get(Calendar.YEAR) - year);
         aluno.setName("Caetano");
-        aluno.setBornDate(2010, 8,16); // JULHO = 8 (os meses são +1)
 
         System.out.println("Name: "+ aluno.getName() +
                            "\nAge: " + aluno.getAge() +
